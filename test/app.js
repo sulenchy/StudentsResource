f3a726f0e31b917cd2ee63b8a66c236dab8b3daa
sulenchy <sulaimanabiodun172@gmail.com>
2017-11-05 07:35:03 +0100

    ch(setup the app.js):prepares the app.js

--- test/app.js
+++ test/app.js
@@ -1,13 +1,20 @@
-import http from 'http';
-import assert from 'assert';
+import express from 'express';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
 
-import '../lib/app.js';
+// Set up the express app
+const app = express();
 
-describe('Example Node Server', () => {
-  it('should return 200', (done) => {
-    http.get('http://127.0.0.1:1337', (res) => {
-      assert.equal(200, res.statusCode);
-      done();
-    });
-  });
-});
+// Log requests to the console.
+app.use(logger('dev'));
+
+// Parse incoming requests data (https://github.com/expressjs/body-parser)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Setup a default catch-all route that sends back a welcome message in JSON format.
+app.get('*', (req, res) => res.status(200).send({
+  message: 'Welcome to the beginning of nothingness.',
+}));
+
+export default app;
